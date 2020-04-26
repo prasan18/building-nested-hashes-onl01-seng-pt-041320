8136deb67c53c1b161db816745ed031fffd097e3
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-26 03:10:59 +0000

    Automatically backed up by Learn

--- lib/bonus.rb
+++ lib/bonus.rb
@@ -21,7 +21,7 @@ def bonus
   }
 
 epic_tragedy[:montague][:hero][:status] = "dead"
- epic_tragedy[:montague][:hero][:status] = "dead"
+ epic_tragedy[:][:hero][:status] = "dead"
 puts epic_tragedy
  
 #  =>
